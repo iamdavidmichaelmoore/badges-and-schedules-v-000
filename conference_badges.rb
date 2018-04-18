7f01e01f805db48c667bfa1b300c9c2dba2b4868
David Moore <hey@iamdavidmoore.com>
2018-04-18 15:25:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,17 +4,23 @@ def badge_maker(name)
 end
 
 #takes an array of speaker's names and creates a badge for each in an array. Contains the #badge_maker as a helper method
-def batch_badge_creator(array)
+def batch_badge_creator(list)
   badged_speakers = []
-  array.each do |speaker_name|
+  list.each do |speaker_name|
     badged_speakers << badge_maker(speaker_name)
   end
-  badged_speakers
+  badged_speakers()
 end
 
 
-def assign_rooms
-
+def assign_rooms(list)
+  room_assignments = []
+  num_rooms = 0
+  list.each do |speaker_name|
+    do while num_rooms < list.length
+      room_assignments << "Hi #{speaker_name}! You'll be assigned to room #{num_rooms}!"
+    end
+  end
 end
 
 def printer
