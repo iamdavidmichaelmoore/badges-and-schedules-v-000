c6694e4e0f58f56457ff512424d9fb3a9808f335
David Moore <hey@iamdavidmoore.com>
2018-04-18 15:11:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(array)
+  badged_speakers = []
+  array.each do |speaker_name|
+    badged_speakers << speaker_name
+  end
+  badged_speakers
+end
+
+
+
+def assign_rooms
+
+end
+
+def printer
+
+end 
